ff7a4f234cd5d68bc79326f895a174d259be2f86
williansscd <williansscd@gmail.com>
2022-07-11 22:35:05 -0300

    modified: all

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Divisao
+using division.classes;
+
+var pessoa = new Pessoa();
+pessoa.ID = 1;
+pessoa.Nome = "Willian Silva";
+pessoa.Phone = "(73)98805-8828";
+
+
+Console.Write($"{pessoa.Nome} Digite a quantidade de divisões: ");
+int limit = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < limit; i++)
+{
+    Console.Write("Digite os dois valores separados por vírgula: ");
+
+    string[] line = Console.ReadLine().Split(",");
+    double x = double.Parse(line[0]);
+    double y = double.Parse(line[1]);
+    
+    if (y != 0)
+    {
+        double resultado = x / y;
+        Console.WriteLine("Resultado: {0:0.0}", resultado);
+    }
+    else
+    {
+        Console.WriteLine("impossivel de realizar a operacao");
+    }
+}
+
+Console.ReadKey();
---
+++ classes/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace division.classes
+{
+    public class Pessoa
+    {
+        public int ID { get; set; }
+        public string? Nome { get; set; }
+        public string? Phone { get; set; }
+    }
+}
